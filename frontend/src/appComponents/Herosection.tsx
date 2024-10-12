eb47a047c42c542f3ae765cb781047a364570b68
Aastha <aasthasinghddun289@gmail.com>
2024-10-12 19:05:23 +0530

    FIxed bug in Herosection

--- frontend/src/appComponents/Herosection.tsx
+++ frontend/src/appComponents/Herosection.tsx
@@ -1,7 +1,5 @@
 "use client";
-
 const Hero: React.FC = () => {
-
   return (
     <div className="relative z-10 flex flex-col w-full overflow-hidden text-white bg-background md:shadow-xl">
       <div className="flex flex-col items-center justify-start py-24">
@@ -14,15 +12,15 @@ const Hero: React.FC = () => {
           lasting connections.
         </p>
         <div className="flex flex-col justify-center gap-4 pt-12 md:flex-row text-slate-200">
-          <button className="h-12 px-6 py-2 font-semibold text-black bg-white rounded-md hover:scale-105 transform transition-all duration-500 hover:text-white hover:bg-black hover:border ">
+          <button className="h-12 px-6 py-2 font-semibold text-black bg-white rounded-md hover:scale-105 transform transition-all duration-500 hover:text-white hover:bg-black hover:border">
             Explore Clubs
           </button>
-          <button className="h-12 px-6 py-2 font-semibold border border-white rounded-md hover:scale-105 transform transition-all duration-500 hover:text-black hover:bg-white hover:border ">
+          <button className="h-12 px-6 py-2 font-semibold border border-white rounded-md hover:scale-105 transform transition-all duration-500 hover:text-black hover:bg-white hover:border">
             Upcoming Events
           </button>
         </div>
       </div>
-</div>
+    </div>
   );
 };
 
