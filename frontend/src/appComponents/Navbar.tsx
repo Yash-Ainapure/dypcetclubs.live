55cf28b6eee2b547fc008c9bf9acbbb00debe7e4
SAGI VENKATA NAGA GOPAL VARMA <138625491+varma-101@users.noreply.github.com>
2024-10-15 01:22:07 +0530

    Changed ui in mobile devices such that menu  button wont collide with club login button, club login btn is placed inside the hamburger menu. (#91)
    * Changed ui in mobile devices such that menu  button wont collide with with club login button,club login btn is placed inside the hamburger menu.
    
    * Solved hamburger menu and club login collision and reduced size of navlinks on mobile devices.

--- frontend/src/appComponents/Navbar.tsx
+++ frontend/src/appComponents/Navbar.tsx
@@ -9,10 +9,11 @@ export type IconProps = React.HTMLAttributes<SVGElement>;
 
 const Navbar: React.FC<any> = ({ setShowLoginPage }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
   return (
     <div className="relative flex items-center justify-between w-full px-4 py-4 text-slate-300 md:h-28 sm:px-6 lg:px-8">
       {/* image-logo */}
@@ -26,46 +27,35 @@ const Navbar: React.FC<any> = ({ setShowLoginPage }) => {
           dypcetclubs.live
         </h2>
       </div>
-      <div className=" transform bg-[#060f1c] bg-gradient-to-b from-[rgb(10,17,27)] to-[#010a18] sticky top-6 transition-all duration-500 hover:left-[34%] left-[35%] w-[550px] hover:w-[600px]">
+      <div className="transform bg-[#060f1c] bg-gradient-to-b from-[rgb(10,17,27)] to-[#010a18] sticky top-6 transition-all duration-500 hover:left-[34%] left-[35%] w-[550px] hover:w-[600px] hidden md:block">
         <Dock
           direction="middle"
           magnification={60}
           distance={100}
-          className="hidden lg:flex items-center w-full text-base border rounded-lg shadow-xl bg-white/10 backdrop-blur-sm border-white/20"
+          className="flex items-center w-full text-base border rounded-lg shadow-xl bg-white/10 backdrop-blur-sm border-white/20"
         >
           <DockIcon>
-            <p className="flex items-center font-semibold transition-all duration-300 transform size-full"
-            onClick={()=>{
-              navigate("/")
-            }}>
+            <p className="flex items-center font-semibold transition-all duration-300 transform size-full" onClick={() => navigate("/")}>
               Home
             </p>
           </DockIcon>
           <DockIcon>
-            <p onClick={()=>{
-              navigate("/clubs")
-            }} className="flex items-center font-semibold transition-all duration-300 transform size-full">
+            <p onClick={() => navigate("/clubs")} className="flex items-center font-semibold transition-all duration-300 transform size-full">
               Clubs
             </p>
           </DockIcon>
           <DockIcon>
-            <p onClick={()=>{
-              navigate("/events")
-            }} className="flex items-center font-semibold transition-all duration-300 transform size-full">
+            <p onClick={() => navigate("/events")} className="flex items-center font-semibold transition-all duration-300 transform size-full">
               Events
             </p>
           </DockIcon>
           <DockIcon>
-            <p onClick={()=>{
-              navigate("/hiring")
-            }} className="flex items-center font-semibold transition-all duration-300 transform size-full">
+            <p onClick={() => navigate("/hiring")} className="flex items-center font-semibold transition-all duration-300 transform size-full">
               Hiring
             </p>
           </DockIcon>
           <DockIcon>
-            <p onClick={()=>{
-              navigate("/about")
-            }} className="flex items-center font-semibold transition-all duration-300 transform size-full">
+            <p onClick={() => navigate("/about")} className="flex items-center font-semibold transition-all duration-300 transform size-full">
               About
             </p>
           </DockIcon>
@@ -82,9 +72,8 @@ const Navbar: React.FC<any> = ({ setShowLoginPage }) => {
         </button>
       </div>
 
-
       {isMenuOpen && (
-        <div className="fixed inset-0 bg-white flex flex-col items-center justify-center text-6xl font-bold font-pixeboy z-[1001]">
+        <div className="fixed inset-0 bg-white flex flex-col items-center justify-center text-2xl font-bold font-pixeboy z-[1001] md:text-6xl text-black">
           <button className="absolute top-5 right-5" onClick={toggleMenu}>
             <img
               src={cross}
@@ -94,20 +83,22 @@ const Navbar: React.FC<any> = ({ setShowLoginPage }) => {
             />
           </button>
 
-          <ul className="flex flex-col gap-8 mt-12 text-black">
-            <li className="hover:underline cursor-pointer text-center ">Home</li>
-            <li className="hover:underline cursor-pointer text-center ">Clubs</li>
-            <li className="hover:underline cursor-pointer text-center ">Events</li>
-            <li className="hover:underline cursor-pointer text-center ">Hiring</li>
-            <li className="hover:underline cursor-pointer text-center ">About</li>
+          <ul className="flex flex-col gap-4 mt-12 md:gap-8">
+            <li className="hover:underline cursor-pointer text-center" onClick={() => { navigate("/"); toggleMenu(); }}>Home</li>
+            <li className="hover:underline cursor-pointer text-center" onClick={() => { navigate("/clubs"); toggleMenu(); }}>Clubs</li>
+            <li className="hover:underline cursor-pointer text-center" onClick={() => { navigate("/events"); toggleMenu(); }}>Events</li>
+            <li className="hover:underline cursor-pointer text-center" onClick={() => { navigate("/hiring"); toggleMenu(); }}>Hiring</li>
+            <li className="hover:underline cursor-pointer text-center" onClick={() => { navigate("/about"); toggleMenu(); }}>About</li>
+            <li className="hover:underline cursor-pointer text-center" onClick={() => { setShowLoginPage(true); toggleMenu(); }}>Club Login</li>
           </ul>
         </div>
       )}
+
       <div
         onClick={() => {
           setShowLoginPage(true);
         }}
-        className="mr-[15vw] lg:mr-0 text-base rounded-md shadow-xl text-black bg-white border-white border p-2 font-semibold cursor-pointer hover:scale-105 transform transition-all duration-500 hover:bg-transparent hover:text-white"
+        className="mr-[15vw] lg:mr-0 text-base rounded-md shadow-xl text-black bg-white border-white border p-2 font-semibold cursor-pointer hover:scale-105 transform transition-all duration-500 hover:bg-transparent hover:text-white md:block hidden"
       >
         Club login
       </div>
@@ -188,7 +179,7 @@ const Navigation = () => {
 
       {/* Mobile menu */}
       {isMenuOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-90 flex flex-col items-center justify-center text-6xl font-bold font-pixeboy z-[1001] text-white">
+        <div className="fixed inset-0 bg-black bg-opacity-90 flex flex-col items-center justify-center text-2xl font-bold font-pixeboy z-[1001] md:text-6xl text-white">
           <button className="absolute top-5 right-5" onClick={toggleMenu}>
             <img
               src={cross}
@@ -198,17 +189,19 @@ const Navigation = () => {
             />
           </button>
 
-          <ul className="flex flex-col gap-8 mt-12">
+          <ul className="flex flex-col gap-4 mt-12 md:gap-8">
             <li onClick={() => { navigate("/"); toggleMenu(); }} className="hover:underline cursor-pointer text-center">Home</li>
             <li onClick={() => { navigate("/clubs"); toggleMenu(); }} className="hover:underline cursor-pointer text-center">Clubs</li>
             <li onClick={() => { navigate("/events"); toggleMenu(); }} className="hover:underline cursor-pointer text-center">Events</li>
             <li onClick={() => { navigate("/hiring"); toggleMenu(); }} className="hover:underline cursor-pointer text-center">Hiring</li>
             <li onClick={() => { navigate("/about"); toggleMenu(); }} className="hover:underline cursor-pointer text-center">About</li>
+            <li onClick={() => { navigate("/club-login"); toggleMenu(); }} className="hover:underline cursor-pointer text-center">Club Login</li>
           </ul>
         </div>
       )}
     </div>
   );
 };
+
 
 export { Navbar,Navigation };
