b3901d95975744229c17b968368af3c30a91648c
Laxmi Ray <122423386+Laxmi01345@users.noreply.github.com>
2024-10-15 16:41:56 +0530

    Enhancements to Event Creation and Management UI/UX (#96)
    * updated
    
    * ehanced UI

--- backend/dist/routes/event.routes.js
+++ backend/dist/routes/event.routes.js
@@ -6,6 +6,7 @@ const router = (0, express_1.Router)();
 router.delete("/deleteEvent", event_controller_1.deleteEvent);
 //get all events
 router.get("/getAllEventData", event_controller_1.getAllEventData);
+router.get("/getSingleEventData", event_controller_1.getSingleEventData);
 //get all events for a club
 router.get("/getClubEventData", event_controller_1.getClubEventData);
 router.put("/update-event", event_controller_1.updateEvent);
