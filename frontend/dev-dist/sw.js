9e0a2c956905f398b425720ec175ff323a82ee65
Yash-Ainapure <ainapureyash2@gmail.com>
2024-11-11 09:50:51 +0530

    disabled pwa for now

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-56da8dea'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qaokcgpg0eg"
+    "revision": "0.hs25it1idog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
