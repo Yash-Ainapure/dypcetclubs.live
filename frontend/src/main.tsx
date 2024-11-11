6a40c8af22c432078950f66aa206ad11ec4b4680
Yash-Ainapure <ainapureyash2@gmail.com>
2024-11-11 09:41:34 +0530

    disabled pwa for now

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>j
+      <AuthProvider>
         <App />
       </AuthProvider>
     </QueryClientProvider>
