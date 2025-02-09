f6d22956656aa78a82f943edbfb3db9a1b758ed0
Yash-Ainapure <ainapureyash2@gmail.com>
2025-02-09 18:01:35 +0530

    removed logs

--- frontend/src/appComponents/QuizCreation.tsx
+++ frontend/src/appComponents/QuizCreation.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import axios from "./axiosInstance";
 import { useAuth } from "../context/AuthContext";
 import microBG from "../assets/micro1.avif";
-import { dA } from "node_modules/@fullcalendar/core/internal-common";
 
 interface Question {
   question: string;
@@ -163,7 +162,7 @@ const QuizCreation: React.FC = () => {
                   />
                 ))}
               </div>
-              <div className="flex gap-2 justify-center items-center bg-white rounded-md pl-2 bg-green-400">
+              <div className="flex gap-2 justify-center items-center rounded-md pl-2 bg-green-400">
                 <p className="font-semibold rounded-md">Answer: </p>
                 <input
                   type="text"
@@ -287,18 +286,3 @@ const AiQuizCreationModal = ({ setQuiz, setQuestions }: any) => {
   );
 };
 export default QuizCreation;
-
-const data = {
-  questions: [
-    {
-      question: "What is GitHub?",
-      options: [
-        "A version control system",
-        "A social networking platform for developers",
-        "A code hosting platform",
-        "All of the above",
-      ],
-      answer: "All of the above",
-    },
-  ],
-};
